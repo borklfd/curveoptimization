d8bc028d4242932ff93f9a9f2fe2643d72123e89
borklfd <67602409+borklfd@users.noreply.github.com>
2023-09-25 10:09:27 +0200

    Rewrite main.py

--- main.py
+++ main.py
@@ -1,52 +1,92 @@
-import math # will be changed to numpy at some point
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-class Segment():
-    def __init__(self, p0, p1, v): #p0, p1 are lists
-        self.p0 = p0.copy()
-        self.p1 = p1.copy()
-        self.v = v
-        self.len = self.length(p0, p1)
-        self.ang = self.angle()
-        
-        
+#suppose that gravity is g=1 ms-2 and the object has a mass of m=1 kg
+class Segment:
+    def __init__(self, p0, p1):
+        self.p0 = p0
+        self.p1 = p1
+        self.len = self.length()
+        self.v = self.vx(p0)
+
+
+    def total_velocity(self, p):
+        return np.sqrt(20-2*p)
     def length(self):
-        """Calculates the length of segment"""
-        return math.sqrt((p1[0]-p0[0])**2+(p1[1]-p0[1])**2)
+        return np.sqrt((self.p0-self.p1)**2+1)
 
     def angle(self):
-        """Calculates the beesési szög"""
-        return math.acos(1/self.len)
-
-    def transition(self, p2):
-        """Assumes the properties of the new segment"""
-        self.p0 = self.p1.copy()
-        self.p1 = p2.copy()
-        self.len = self.length()
-        newang = self.angle()
-        self.v = math.sin(newang)*self.v/math.sin(self.ang)
-        self.ang = newang
+        return np.arccos(1/self.length())
 
     def time(self):
-        """Calculate the time needed to travrese the segment"""
-        return self.len/self.v
+        return 1/self.v
 
-###################################################################################################
-#testing area, will be deleted at some point
-points = []
-for i in range(10, -1, -1):
-    points.append(i)
-print(points)
-segment = Segment([0, points[0]], [1, points[1]], 10)
-time = 0
-for i in range(2, len(points)):
-    time += segment.time()
-    print("Segment no. {n}, time = {t}".format(n=i, t=segment.time()))
-    print("angle = {a}".format(a=segment.ang))
-    print(segment.p0)
-    print(segment.p1)
-    print(segment.len)
-    segment.transition([i, points[i]])
-    
-    
-print("time =", time)
+    def vx(self, p):
+        return self.total_velocity(p)/self.len
 
+    def transition(self, p2):
+        p0 = self.p1
+        p1 = p2
+        self.p0 = p0
+        self.p1 = p1
+        self.len = self.length()
+        self.v = self.vx(p0)
+        return self.time()
+
+
+
+def compare(t1, t2):
+    diff = t1-t2
+    return diff
+
+def goodsqrt(x):
+    if x < 0:
+        return -np.sqrt(-x)
+    return np.sqrt(x)
+
+st = np.array([])
+a = np.array([10.0])
+for i in range(10):
+    a = np.append(a, 9.0-i)
+a_1 = a.copy()
+for p in range(1, 9):
+    a[p] += np.random.rand()*2-1
+#plt.plot(a)
+#plt.show()
+iterations = np.array([])
+for i in range(20):
+    s = Segment(a[0], a[1])
+    iterations = np.append(iterations, s)
+t = np.array([])
+for i in range(10):
+    t = np.append(t, 1)
+t_1 = t.copy()
+
+for i in iterations:
+    diff_t = compare(t_1, t)
+    diff_a = compare(a_1, a)
+    t_1 = t.copy()
+    t = np.array([10])
+
+    for p in range(1, 9):
+        if diff_a[p]*diff_t[p]>0:
+            a[p] -= diff_t[p]/goodsqrt(diff_a[p])*np.random.rand()*0.5
+        elif diff_a[p] != 0:
+            a[p] += diff_t[p]/goodsqrt(diff_a[p])*np.random.rand()*0.5
+        else:
+            a[p] += np.random.rand()*0.5
+    plt.plot(a)
+    plt.show()
+    i.p1 = a[1]
+    for j in a[2:]:
+        dt = i.transition(j)
+        t = np.append(t, dt)
+    st = np.append(st, t.sum())
+    time.sleep(0.1)
+
+
+###########################################
+
+
+print(st)
