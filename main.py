277f83815238c569bbc02090aa564afa6e3db7d2
borklfd <67602409+borklfd@users.noreply.github.com>
2023-11-02 16:36:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from segment import Segment
 import time
 
 #suppose that gravity is g=1 ms-2 and the object has a mass of m=1 kg
-class Segment:
-    def __init__(self, p0, p1):
-        self.p0 = p0
-        self.p1 = p1
-        self.len = self.length()
-        self.v = self.vx(p0)
-
-
-    def total_velocity(self, p):
-        return np.sqrt(20-2*p)
-    def length(self):
-        return np.sqrt((self.p0-self.p1)**2+1)
-
-    def angle(self):
-        return np.arccos(1/self.length())
-
-    def time(self):
-        return 1/self.v
-
-    def vx(self, p):
-        return self.total_velocity(p)/self.len
-
-    def transition(self, p2):
-        p0 = self.p1
-        p1 = p2
-        self.p0 = p0
-        self.p1 = p1
-        self.len = self.length()
-        self.v = self.vx(p0)
-        return self.time()
 
 
 
@@ -45,48 +16,55 @@ def goodsqrt(x):
         return -np.sqrt(-x)
     return np.sqrt(x)
 
+
 st = np.array([])
-a = np.array([10.0])
-for i in range(10):
-    a = np.append(a, 9.0-i)
+a = np.array([100.0])
+
+for i in range(100):
+    a = np.append(a, 99.0-i)
 a_1 = a.copy()
-for p in range(1, 9):
+
+for p in range(1, 99):
     a[p] += np.random.rand()*2-1
-#plt.plot(a)
-#plt.show()
+plt.plot(a)
+plt.show()
 iterations = np.array([])
-for i in range(20):
+
+for i in range(1000):
     s = Segment(a[0], a[1])
     iterations = np.append(iterations, s)
 t = np.array([])
-for i in range(10):
+
+for i in range(100):
     t = np.append(t, 1)
 t_1 = t.copy()
 
-for i in iterations:
-    diff_t = compare(t_1, t)
-    diff_a = compare(a_1, a)
-    t_1 = t.copy()
-    t = np.array([10])
 
-    for p in range(1, 9):
+for i in iterations:
+    diff_t = compare(t, t_1)
+    diff_a = compare(a, a_1)
+    t_1 = t.copy()
+    t = np.array([100])
+
+    for p in range(1, 99):
         if diff_a[p]*diff_t[p]>0:
-            a[p] -= diff_t[p]/goodsqrt(diff_a[p])*np.random.rand()*0.5
-        elif diff_a[p] != 0:
-            a[p] += diff_t[p]/goodsqrt(diff_a[p])*np.random.rand()*0.5
+            a[p] -= goodsqrt(diff_t[p]/diff_a[p])*np.random.normal(0.5, 0.2)
+        elif diff_t[p] != 0:
+            a[p] += goodsqrt(diff_t[p]/diff_a[p])*np.random.normal(0.5, 0.2)
         else:
-            a[p] += np.random.rand()*0.5
-    plt.plot(a)
-    plt.show()
+            a[p] += 0
+
+    #plt.plot(a)
+    #plt.show()
     i.p1 = a[1]
     for j in a[2:]:
         dt = i.transition(j)
         t = np.append(t, dt)
     st = np.append(st, t.sum())
-    time.sleep(0.1)
 
 
 ###########################################
-
+plt.plot(a)
+plt.show()
 
 print(st)
